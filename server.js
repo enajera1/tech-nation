28ec0e4b8f25681efb6e9b271e53a5702f168e83
enajera1 <eriknajera013@gmail.com>
2022-08-07 17:38:32 -0500

    post feature almost set up

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const routes = require('./controllers'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,21 +20,20 @@ const sess = {
     db: sequelize
   })
 };
-
-const helpers = require('./utils/helpers');
-
-app.use(session(sess));
-
+// helper function 
 const hbs = exphbs.create({ helpers });
+
  
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+//turn on routes
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`
